9d6438f79a360e61474a80c0fab3a186546cff5e
L00171222 <L00171222@atu.ie>
2023-03-31 20:26:41 +0100

    ChatGPT 1 pipeline actions 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to github actions.
+          Edit <code>src/App.js</code> ChatGPT pipeline code running in github actions.
         </p>
         <a
           className="App-link"
